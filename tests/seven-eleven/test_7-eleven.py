b4b8e6e324a2760c5c82ac0812003cf3ca01e0a6
Chinnaphat <chinnaphat46@gmail.com>
2024-01-03 08:44:09 +0700

    Initial code.

---
+++ tests/seven-eleven/test_7-eleven.py
@@ -0,0 +1,13 @@
+"""Test cases for 7-eleven"""
+
+# Standard library
+
+# Third party library
+import pytest
+
+# Project library
+# from seven_eleven.seven import print_7_eleven
+
+
+def test_print_7_eleven():
+    """Print 7-eleven"""
